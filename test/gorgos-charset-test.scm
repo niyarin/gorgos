6055e04f31e67ab073521caf467fd01d9022b06e
niyarin <inoanet@gmail.com>
2020-11-14 21:41:16 +0900

    Add notcharset.

--- test/gorgos-charset-test.scm
+++ test/gorgos-charset-test.scm
@@ -7,7 +7,7 @@
         (gorgos core)
         (gorgos charset))
 
-(begin;;gchar-test
+(begin;;gcharset-test
    (test-begin "gcharset-test")
    (let-values (((v next) ((gcharset (char-set #\t #\a)) "test")))
       (test-eq v #\t)
@@ -17,3 +17,12 @@
       (test-assert (gfail-object? v)))
    (test-end "gcharset-test"))
 
+(begin;;gnotchar-test
+   (test-begin "gnotcharset-test")
+   (let-values (((v next) ((gnotcharset (char-set #\a #\b)) "test")))
+      (test-eq v #\t)
+      (test-equal next "est"))
+
+   (let-values (((v next) ((gnotcharset (char-set #\t)) "test")))
+      (test-assert (gfail-object? v)))
+   (test-end "gnotcharset-test"))
