8261c7b13f6364a7aa2788327d4a60192bcb5f41
niyarin <inoanet@gmail.com>
2020-11-15 08:18:34 +0900

    Add calculator sample

---
+++ examples/calculator.scm
@@ -0,0 +1,51 @@
+(include "../src/gorgos-core.scm")
+(include "../src/gorgos-charset.scm")
+(include "../src/gorgos-advance.scm")
+(include "../src/gorgos-utils.scm")
+
+(define-library (gorgos-sample calculator)
+   (import (scheme base) (scheme charset)
+           (gorgos core) (gorgos charset) (gorgos advance) (gorgos utils))
+   (export expr-parser)
+   (begin
+     (define expr-parser
+       (lambda (x)
+         ((gconv (gpair term-parser
+                        (goptional (glist (gor (gchar #\+) (gchar #\-)) expr-parser)))
+          (lambda (res)
+            (cond
+              ((null? (cdr res)) (car res))
+              ((eq? (cadr res) #\+) (+ (car res) (list-ref res 2)))
+              ((eq? (cadr res) #\-) (- (car res) (list-ref res 2)))
+              (else res))))
+          x)))
+
+     (define term-parser
+       (lambda (x)
+         ((gconv (gpair factor-parser
+                        (goptional (glist (gor (gchar #\*) (gchar #\/)) term-parser)))
+          (lambda (res)
+            (cond
+              ((null? (cdr res)) (car res))
+              ((eq? (cadr res) #\*) (* (car res) (list-ref res 2)))
+              ((eq? (cadr res) #\/) (/ (car res) (list-ref res 2)))
+              (else res))))
+          x)))
+
+     (define unsigned-integer-parser
+        (gconv (gpair (gcharset char-set:digit)
+                      (glist-of (gcharset char-set:digit)))
+               (lambda (x) (string->number (list->string x)))))
+
+     (define paren-parser
+       (gconv (glist (gchar #\()
+                     expr-parser
+                     (gchar #\)))
+              cadr))
+
+     (define factor-parser
+       (gor unsigned-integer-parser paren-parser))))
+
+;(import (scheme base) (scheme write) (gorgos-sample calculator))
+;(let-values (((v next) (expr-parser "1+2*(3+4)-21/7")))
+;  (write v)(newline))
